5d4a1a201c5a74b0e14c77cf2f19bbfd657fb2ab
Fombi Favour <fombifavour@gmail.com>
2024-02-15 12:29:52 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     authenticated :user do
       root 'categories#index', as: :authenticated_root
+      get '/users/sign_out' => 'devise/sessions#destroy'
     end
     unauthenticated do
       root 'splash#home', as: :unauthenticated_root

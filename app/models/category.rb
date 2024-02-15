edf4baee3049079713490a01363c35140afc0b43
Fombi Favour <fombifavour@gmail.com>
2024-02-15 08:44:29 +0100

    Update category.rb

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,12 @@ class Category < ApplicationRecord
   has_and_belongs_to_many :expenses
 
   validates :name, :icon, presence: true
+
+  def total
+    expenses.sum(:amount)
+  end
+
+  def recent_expenses
+    expenses.order(created_at: :desc)
+  end
 end
